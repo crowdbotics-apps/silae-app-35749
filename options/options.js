122dbc3042e09bc13411e645d964ce7df967ef51
Crowdbotics <team@crowdbotics.com>
2022-06-16 09:16:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "silae_app_35749",
+  url: "https://silae_app_35749.botics.co",
+  api: "https://silae_app_35749.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
